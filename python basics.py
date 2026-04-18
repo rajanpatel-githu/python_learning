8574d83ff3c7f7bc61fabf988f2edd05d5e467dd
rajanpatel-githu <rpchaudhary448@gmail.com>
2026-04-18 13:34:44 +0530

    Day 1: Python Basics
    Variables, input/output and basic addition program

---
+++ python basics.py
@@ -0,0 +1,24 @@
+# Day 1: Python Basics
+
+# 1. Variables
+name = "Rajan"
+age = 25
+
+print("My name is", name)
+print("My age is", age)
+
+# 2. Taking input from user
+user_name = input("Enter your name: ")
+print("Welcome", user_name)
+
+# 3. Simple calculation
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum_result = num1 + num2
+print("Sum is:", sum_result)
+
+# 4. Data types check
+print(type(name))
+print(type(age))
+print(type(sum_result))
